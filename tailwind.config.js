f7f4b2c60405bf850d73baadf39904a830fc3953
Ashu Singh <ashusingh@Ashus-MacBook-Air.local>
2024-11-15 16:59:15 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Add this line to specify all files inside src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
